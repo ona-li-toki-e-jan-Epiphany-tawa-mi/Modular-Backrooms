0ce08057b2701ec746033f254b850b5a00d75821
ona-li-toki-e-jan-Epiphany-tawa-mi <darklorddudeal.deaddear@gmail.com>
2023-02-25 02:21:28 -0500

    Added yellowed wall paper walls. Switch creative menu icon to wallpaper.

--- src/main/java/net/epiphany/mdlrbckrms/blocks/YellowedWallpaper.java
+++ src/main/java/net/epiphany/mdlrbckrms/blocks/YellowedWallpaper.java
@@ -8,6 +8,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.block.StairsBlock;
+import net.minecraft.block.WallBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -46,6 +47,16 @@ public class YellowedWallpaper  {
     public static final BlockItem YELLOWED_WALLPAPER_STAIRS_ITEM = new BlockItem( YELLOWED_WALLPAPER_STAIRS
                                                                                 , new FabricItemSettings());
 
+    /**
+     * Wall variant of the player craftable wallpaper.
+     */
+    public static final Identifier YELLOWED_WALLPAPER_WALL_ID = new Identifier( ModularBackrooms.MOD_ID
+                                                                              , "yellowed_wallpaper_wall");
+    public static final WallBlock YELLOWED_WALLPAPER_WALL = 
+            new WallBlock(FabricBlockSettings.copyOf(YELLOWED_WALLPAPER));
+    public static final BlockItem YELLOWED_WALLPAPER_WALL_ITEM = new BlockItem( YELLOWED_WALLPAPER_WALL
+                                                                              , new FabricItemSettings());
+
 
 
     /**
@@ -78,6 +89,16 @@ public class YellowedWallpaper  {
                            , FabricBlockSettings.copyOf(UNBREAKABLE_YELLOWED_WALLPAPER));
     public static final BlockItem UNBREAKABLE_YELLOWED_WALLPAPER_STAIRS_ITEM = 
             new BlockItem(UNBREAKABLE_YELLOWED_WALLPAPER_STAIRS, new FabricItemSettings());
+
+    /**
+     * Wall variant of the indestructable wallpaper.
+     */
+    public static final Identifier UNBREAKABLE_YELLOWED_WALLPAPER_WALL_ID = 
+        new Identifier(ModularBackrooms.MOD_ID, "unbreakable_yellowed_wallpaper_wall");
+    public static final WallBlock UNBREAKABLE_YELLOWED_WALLPAPER_WALL = 
+            new WallBlock(FabricBlockSettings.copyOf(UNBREAKABLE_YELLOWED_WALLPAPER));
+    public static final BlockItem UNBREAKABLE_YELLOWED_WALLPAPER_WALL_ITEM = 
+            new BlockItem( UNBREAKABLE_YELLOWED_WALLPAPER_WALL, new FabricItemSettings());
     
 
 
@@ -91,6 +112,9 @@ public class YellowedWallpaper  {
         Registry.register(Registries.BLOCK, YELLOWED_WALLPAPER_STAIRS_ID, YELLOWED_WALLPAPER_STAIRS);
         Registry.register(Registries.ITEM, YELLOWED_WALLPAPER_STAIRS_ID, YELLOWED_WALLPAPER_STAIRS_ITEM);
 
+        Registry.register(Registries.BLOCK, YELLOWED_WALLPAPER_WALL_ID, YELLOWED_WALLPAPER_WALL);
+        Registry.register(Registries.ITEM, YELLOWED_WALLPAPER_WALL_ID, YELLOWED_WALLPAPER_WALL_ITEM);
+
         
         Registry.register(Registries.BLOCK, UNBREAKABLE_YELLOWED_WALLPAPER_ID, UNBREAKABLE_YELLOWED_WALLPAPER);
         Registry.register(Registries.ITEM, UNBREAKABLE_YELLOWED_WALLPAPER_ID, UNBREAKABLE_YELLOWED_WALLPAPER_ITEM);
@@ -100,15 +124,20 @@ public class YellowedWallpaper  {
 
         Registry.register(Registries.BLOCK, UNBREAKABLE_YELLOWED_WALLPAPER_STAIRS_ID, UNBREAKABLE_YELLOWED_WALLPAPER_STAIRS);
         Registry.register(Registries.ITEM, UNBREAKABLE_YELLOWED_WALLPAPER_STAIRS_ID, UNBREAKABLE_YELLOWED_WALLPAPER_STAIRS_ITEM);
+
+        Registry.register(Registries.BLOCK, UNBREAKABLE_YELLOWED_WALLPAPER_WALL_ID, UNBREAKABLE_YELLOWED_WALLPAPER_WALL);
+        Registry.register(Registries.ITEM, UNBREAKABLE_YELLOWED_WALLPAPER_WALL_ID, UNBREAKABLE_YELLOWED_WALLPAPER_WALL_ITEM);
     }
 
     public static void registerBlockItemUnderGroup(FabricItemGroupEntries content) {
         content.add(YELLOWED_WALLPAPER_ITEM);
         content.add(YELLOWED_WALLPAPER_SLAB_ITEM);
         content.add(YELLOWED_WALLPAPER_STAIRS_ITEM);
+        content.add(YELLOWED_WALLPAPER_WALL);
 
         content.add(UNBREAKABLE_YELLOWED_WALLPAPER_ITEM);
         content.add(UNBREAKABLE_YELLOWED_WALLPAPER_SLAB_ITEM);
         content.add(UNBREAKABLE_YELLOWED_WALLPAPER_STAIRS_ITEM);
+        content.add(UNBREAKABLE_YELLOWED_WALLPAPER_WALL);
     }
 }
