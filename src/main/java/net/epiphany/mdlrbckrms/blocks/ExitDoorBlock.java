a667b809fbb93ff7235e2d473d6c838b112731e9
ona-li-toki-e-jan-Epiphany-tawa-mi <darklorddudeal.deaddear@gmail.com>
2023-03-05 11:38:00 -0500

    Added item model and proper hitbox for exit doors

--- src/main/java/net/epiphany/mdlrbckrms/blocks/ExitDoorBlock.java
+++ src/main/java/net/epiphany/mdlrbckrms/blocks/ExitDoorBlock.java
@@ -2,11 +2,14 @@ package net.epiphany.mdlrbckrms.blocks;
 
 import net.epiphany.mdlrbckrms.ModularBackrooms;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.block.Material;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.block.enums.DoorHinge;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
@@ -21,14 +24,23 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-// TODO Add item model
+// TODO switch to using a single separate sound for opening and closing each.
 public class ExitDoorBlock extends DoorBlock {
     /**
      * Whether the exit door is a portal to somewhere else.
      */
     public static final BooleanProperty PORTAL = BooleanProperty.of("portal");
+
+    public ExitDoorBlock(Settings settings, SoundEvent closeSound, SoundEvent openSound) {
+        super(settings, closeSound, openSound);
+        this.setDefaultState(getDefaultState().with(PORTAL, true));
+    }
 
     @Override
     protected void appendProperties(Builder<Block, BlockState> builder) {
@@ -52,21 +64,60 @@ public class ExitDoorBlock extends DoorBlock {
         Registry.register(Registries.ITEM, EXIT_DOOR_ID, EXIT_DOOR_ITEM);
     }
 
-    public ExitDoorBlock(Settings settings, SoundEvent closeSound, SoundEvent openSound) {
-        super(settings, closeSound, openSound);
-        this.setDefaultState(getDefaultState().with(PORTAL, false));
+    public static void registerBlockItemUnderGroup(FabricItemGroupEntries content) {
+        content.add(EXIT_DOOR_ITEM);
+    }
+
+    
+
+    protected static final VoxelShape NORTH_LEFT_OPEN_PORTAL_SHAPE = VoxelShapes.union(DoorBlock.WEST_SHAPE, 
+            Block.createCuboidShape(3.0, 0.0, 12.0, 16.0, 16.0, 15.0));
+    protected static final VoxelShape NORTH_RIGHT_OPEN_PORTAL_SHAPE = VoxelShapes.union(DoorBlock.EAST_SHAPE, 
+            Block.createCuboidShape(0.0, 0.0, 12.0, 13.0, 16.0, 15.0));
+    protected static final VoxelShape SOUTH_LEFT_OPEN_PORTAL_SHAPE = VoxelShapes.union(DoorBlock.EAST_SHAPE, 
+            Block.createCuboidShape(0.0, 0.0, 1.0, 13.0, 16.0, 4.0));
+    protected static final VoxelShape SOUTH_RIGHT_OPEN_PORTAL_SHAPE = VoxelShapes.union(DoorBlock.WEST_SHAPE, 
+            Block.createCuboidShape(3.0, 0.0, 1.0, 16.0, 16.0, 4.0));
+    protected static final VoxelShape EAST_LEFT_OPEN_PORTAL_SHAPE = VoxelShapes.union(DoorBlock.NORTH_SHAPE, 
+            Block.createCuboidShape(1.0, 0.0, 3.0, 4.0, 16.0, 16.0));
+    protected static final VoxelShape EAST_RIGHT_OPEN_PORTAL_SHAPE = VoxelShapes.union(DoorBlock.SOUTH_SHAPE, 
+            Block.createCuboidShape(1.0, 0.0, 0.0, 4.0, 16.0, 13.0));
+    protected static final VoxelShape WEST_LEFT_OPEN_PORTAL_SHAPE = VoxelShapes.union(DoorBlock.SOUTH_SHAPE, 
+            Block.createCuboidShape(12.0, 0.0, 0.0, 15.0, 16.0, 13.0));
+    protected static final VoxelShape WEST_RIGHT_OPEN_PORTAL_SHAPE = VoxelShapes.union(DoorBlock.NORTH_SHAPE,
+            Block.createCuboidShape(12.0, 0.0, 3.0, 15.0, 16.0, 16.0));
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        if (!state.get(PORTAL) || !state.get(DoorBlock.OPEN))
+            return super.getOutlineShape(state, world, pos, context);
+
+
+        Direction facing = state.get(DoorBlock.FACING);
+        boolean hingesRight = state.get(DoorBlock.HINGE) == DoorHinge.RIGHT;
+        
+        switch (facing) {
+            case SOUTH: 
+                return hingesRight ? SOUTH_RIGHT_OPEN_PORTAL_SHAPE : SOUTH_LEFT_OPEN_PORTAL_SHAPE;
+            case WEST: 
+                return hingesRight ? WEST_RIGHT_OPEN_PORTAL_SHAPE : WEST_LEFT_OPEN_PORTAL_SHAPE;
+            case NORTH:
+                return hingesRight ? NORTH_RIGHT_OPEN_PORTAL_SHAPE : NORTH_LEFT_OPEN_PORTAL_SHAPE;
+            default: 
+                return hingesRight ? EAST_RIGHT_OPEN_PORTAL_SHAPE : EAST_LEFT_OPEN_PORTAL_SHAPE;
+        }
     }
 
 
 
     @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos position, PlayerEntity player, Hand hand,
-            BlockHitResult hit) {
+    public ActionResult onUse(BlockState state, World world, BlockPos position, PlayerEntity player, Hand hand
+            , BlockHitResult hit) {
         if (world.isClient)
-            return ActionResult.success(true);
+            ActionResult.success(true);
+
+        this.setOpen(player, world, state, position, !state.get(DoorBlock.OPEN));
         
-        state = state.with(PORTAL, world.getRandom().nextBoolean());
-        this.setOpen(null, world, state, position, !this.isOpen(state));
         return ActionResult.success(false);
 
 
@@ -75,7 +126,7 @@ public class ExitDoorBlock extends DoorBlock {
         if (world.isClient || !hand.equals(Hand.MAIN_HAND))
             return super.onUse(state, world, position, player, hand, hit);
 
-        ModularBackrooms.LOGGER.info("Right click on exit door!");//TODO remove
+        ModularBackrooms.LOGGER.info("Right click on exit door!");//TODO Add portal shit
 
         // Properties of the exit door are dependent upon which dimension they are located inside.
         if (DimensionHelper.isDimension(world, Level0.LEVEL_0_DIMENSION_KEY)) {
