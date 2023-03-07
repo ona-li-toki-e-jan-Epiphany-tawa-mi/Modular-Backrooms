7002d95e9b1b4714d49edbc7e2a086d9707f8fcc
ona-li-toki-e-jan-Epiphany-tawa-mi <darklorddudeal.deaddear@gmail.com>
2023-03-07 09:54:13 -0500

    Added nether portal effects to exit doors, created tempoary portals for them

--- src/main/java/net/epiphany/mdlrbckrms/blocks/ExitDoorBlock.java
+++ src/main/java/net/epiphany/mdlrbckrms/blocks/ExitDoorBlock.java
@@ -2,7 +2,6 @@ package net.epiphany.mdlrbckrms.blocks;
 
 import net.epiphany.mdlrbckrms.ModularBackrooms;
 import net.epiphany.mdlrbckrms.levels.Levels;
-import net.epiphany.mdlrbckrms.utilities.DimensionHelper;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -13,13 +12,17 @@ import net.minecraft.block.DoorBlock;
 import net.minecraft.block.Material;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.enums.DoorHinge;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager.Builder;
@@ -30,11 +33,18 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 
+// TODO Fix issue with exit doors being stubborn when trying to open.
+
+/**
+ * An exit door. Forms one of the main ways of transferring throught the Backrooms.
+ */
 public class ExitDoorBlock extends DoorBlock implements BlockEntityProvider {
     /**
      * Whether the exit door is a portal to somewhere else.
@@ -43,13 +53,38 @@ public class ExitDoorBlock extends DoorBlock implements BlockEntityProvider {
 
     public ExitDoorBlock(Settings settings, SoundEvent closeSound, SoundEvent openSound) {
         super(settings, closeSound, openSound);
-        this.setDefaultState(getDefaultState().with(PORTAL, true));
+        this.setDefaultState(getDefaultState().with(PORTAL, false));
     }
 
     @Override
     protected void appendProperties(Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
         builder.add(PORTAL);
+    }
+
+    /**
+     * Allows changes of the door's portal state to propagate to the other half.
+     */
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState,
+            WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (direction.getAxis() == Direction.Axis.Y && neighborState.isOf(this))
+            state = state.with(PORTAL, neighborState.get(PORTAL));
+                    
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+    @Override
+    public BlockEntity createBlockEntity(BlockPos position, BlockState state) {
+        return new ExitDoorBlockEntity(position, state);
+    }
+
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state,
+            BlockEntityType<T> type) {
+        return type != ExitDoorBlockEntity.EXIT_DOOR_BLOCK_ENTITY ? null : (worldd, position, statee, blockEntity) -> {
+            ExitDoorBlockEntity.tick(worldd, position, statee, (ExitDoorBlockEntity) blockEntity);
+        };
     }
 
 
@@ -94,6 +129,7 @@ public class ExitDoorBlock extends DoorBlock implements BlockEntityProvider {
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        // If the portal isn't being shown we can just render it as a normal door.
         if (!state.get(PORTAL) || !state.get(DoorBlock.OPEN))
             return super.getOutlineShape(state, world, pos, context);
 
@@ -113,30 +149,70 @@ public class ExitDoorBlock extends DoorBlock implements BlockEntityProvider {
         }
     }
 
-
-
+    /**
+     * Modified copy of {@link net.minecraft.block.NetherPortalBlock} to have the same particle effects.
+     */
     @Override
-    public BlockEntity createBlockEntity(BlockPos position, BlockState state) {
-        return new ExitDoorBlockEntity(position, state);
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        if (!state.get(DoorBlock.OPEN) || !state.get(PORTAL))
+            return;
+
+        if (random.nextInt(100) == 0) 
+            world.playSound( pos.getX() + 0.5, (double)pos.getY() + 0.5, (double)pos.getZ() + 0.5
+                           , SoundEvents.BLOCK_PORTAL_AMBIENT, SoundCategory.BLOCKS
+                           , 0.5f, random.nextFloat() * 0.4f + 0.8f, false);
+
+        for (int i = 0; i < 4; ++i) {
+            double randomX = pos.getX() + random.nextDouble();
+            double randomY = pos.getY() + random.nextDouble();
+            double randomZ = pos.getZ() + random.nextDouble();
+            double xOffset = ((double)random.nextFloat() - 0.5) * 0.5;
+            double yOffset = ((double)random.nextFloat() - 0.5) * 0.5;
+            double zOffset = ((double)random.nextFloat() - 0.5) * 0.5;
+            int particleCount = random.nextInt(2) * 2 - 1;
+
+            Direction facing = state.get(DoorBlock.FACING);
+            if (facing == Direction.SOUTH || facing == Direction.NORTH) {
+                randomZ = pos.getZ() + 0.5 + 0.25 * particleCount;
+                zOffset = random.nextFloat() * 2.0f * particleCount;
+            } else {
+                randomX = pos.getX() + 0.5 + 0.25 * particleCount;
+                xOffset = random.nextFloat() * 2.0f * particleCount;
+            }
+            
+            world.addParticle(ParticleTypes.PORTAL, randomX, randomY, randomZ, xOffset, yOffset, zOffset);
+        }
     }
+
+
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos position, PlayerEntity player, Hand hand
             , BlockHitResult hit) {
-        this.setOpen(player, world, state, position, !state.get(DoorBlock.OPEN));
-
         if (world.isClient)
             return ActionResult.success(true);
 
+        this.setOpen(null, world, state, position, !state.get(DoorBlock.OPEN));
+
+        // TODO make this a callback.
         if (Levels.isBackrooms(world)) {
-            ExitDoorBlockEntity blockEntity = world.getBlockEntity(position, ExitDoorBlockEntity.EXIT_DOOR_BLOCK_ENTITY).get();
-            if (!blockEntity.hasPortal())
-                blockEntity.createPortal(world.getServer().getOverworld().getSpawnPos());
+            ExitDoorBlockEntity blockEntity = world.getBlockEntity(position, ExitDoorBlockEntity.EXIT_DOOR_BLOCK_ENTITY)
+                                                   .orElse(null);
+
+            if (blockEntity != null && !blockEntity.hasPortal()) {
+                world.setBlockState(position, state.with(PORTAL, true));
+
+                World overworld = world.getServer().getOverworld();
+                blockEntity.createPortal(overworld.getSpawnPos(), overworld.getRegistryKey());
+            }
         }
         
         return ActionResult.success(false);
     }
 
+    /**
+     * Allows entering the door's portal by colliding with it, should it exist.
+     */
     @Override
     public void onEntityCollision(BlockState state, World world, BlockPos position, Entity entity) {
         if (world.isClient)
@@ -144,12 +220,8 @@ public class ExitDoorBlock extends DoorBlock implements BlockEntityProvider {
         if (!state.get(DoorBlock.OPEN) || !state.get(PORTAL))
             return;
 
-        ExitDoorBlockEntity blockEntity = world.getBlockEntity(position, ExitDoorBlockEntity.EXIT_DOOR_BLOCK_ENTITY).get();
-        
-        if (blockEntity.hasPortal())
-            DimensionHelper.teleportToDimension( entity
-                                               , world.getServer().getOverworld().getRegistryKey()
-                                               , blockEntity.getDestination().toCenterPos()
-                                               , true);
+        ExitDoorBlockEntity blockEntity = world.getBlockEntity(position, ExitDoorBlockEntity.EXIT_DOOR_BLOCK_ENTITY).orElse(null);
+        if (blockEntity != null)
+            blockEntity.tryTeleportEntity(entity);
     }
 }
--- src/main/java/net/epiphany/mdlrbckrms/blocks/ExitDoorBlockEntity.java
+++ src/main/java/net/epiphany/mdlrbckrms/blocks/ExitDoorBlockEntity.java
@@ -3,16 +3,23 @@ package net.epiphany.mdlrbckrms.blocks;
 import org.jetbrains.annotations.Nullable;
 
 import net.epiphany.mdlrbckrms.ModularBackrooms;
+import net.epiphany.mdlrbckrms.utilities.DimensionHelper;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
+/**
+ * A block entity that keeps track of data for the portals within exit doors.
+ */
 public class ExitDoorBlockEntity extends BlockEntity {
     public static final Identifier EXIT_DOOR_BLOCK_ENTITY_ID = new Identifier(ModularBackrooms.MOD_ID, "exit_door_entity");
     public static BlockEntityType<ExitDoorBlockEntity> EXIT_DOOR_BLOCK_ENTITY = null;
@@ -30,16 +37,22 @@ public class ExitDoorBlockEntity extends BlockEntity {
 
     public static final String PORTAL_LIFESPAN_NBT = "PortalLifespan";
     public static final String DESTINATION_NBT = "Destination";
+    public static final String DESTINATION_DIMENSION_NBT = "DestinationDimension";
 
     /**
      * The time in ticks before the portal in the exit door will attempt to close itself.
      */
-    private int portalLifespan = 0;
+    private int portalLifespan = -1;
     /**
      * The destination where any entities entering the exit door will be transported to, or null, if there is none.
      */
     @Nullable
     private BlockPos destination = null; 
+    /**
+     * The world the destination lies in.
+     */
+    @Nullable
+    private RegistryKey<World> destinationDimension = null;
 
     public ExitDoorBlockEntity(BlockPos position, BlockState state) {
         super(EXIT_DOOR_BLOCK_ENTITY, position, state);
@@ -54,6 +67,8 @@ public class ExitDoorBlockEntity extends BlockEntity {
             nbt.putIntArray(DESTINATION_NBT, new int[] { this.destination.getX()
                                                        , this.destination.getY()
                                                        , this.destination.getZ()});
+        if (this.destinationDimension != null) 
+            nbt.putString(DESTINATION_DIMENSION_NBT, destinationDimension.getValue().toString());
     }
 
     @Override
@@ -67,30 +82,93 @@ public class ExitDoorBlockEntity extends BlockEntity {
                                                                          , possibleDestination[1]
                                                                          , possibleDestination[2])
                                                            : null;
+
+        String possibleDestinationDimension = nbt.getString(DESTINATION_DIMENSION_NBT);
+        this.destinationDimension = !possibleDestinationDimension.isEmpty() 
+                ? DimensionHelper.wrapDimensionID(new Identifier(possibleDestinationDimension)) : null;
     }
 
 
 
+    public static void tick(World world, BlockPos position, BlockState state, ExitDoorBlockEntity blockEntity) {
+        if (blockEntity.portalLifespan == 0) {
+            blockEntity.removePortal();
+            // Stops the portal from being displayed as part of the door model.
+            ExitDoorBlock.EXIT_DOOR.setOpen( null
+                                           , world
+                                           , state.with(ExitDoorBlock.PORTAL, false)
+                                           , position
+                                           , false);
+
+        } else if (blockEntity.portalLifespan > 0) 
+            blockEntity.portalLifespan--; 
+    }
+
+
+
+    /**
+     * Attempts to send the entity through the exit door's portal.
+     * 
+     * @param entity The entity to send.
+     * @return {@code null} if the portal does not exist. See {@link DimensionHelper#teleportToDimension(Entity, RegistryKey, BlockPos, boolean)} for more return values.
+     */
+    @Nullable
+    public Entity tryTeleportEntity(Entity entity) {
+        if (!this.hasPortal())
+            return null;
+
+        return DimensionHelper.teleportToDimension( entity
+                                                  , this.destinationDimension
+                                                  , this.getDestination().toCenterPos()
+                                                  , true);
+    }
+
+
+
+    /**
+     * @return Whether the exit door portal exists, as-in it has a destination.
+     */
     public boolean hasPortal() {
         return this.destination != null;
     }
 
+    /**
+     * @return The destination of the exit door portal, or null, if it doesn't exist.
+     */
     @Nullable
     public BlockPos getDestination() {
         return this.destination;
     }
 
-    public void createPortal(BlockPos destination) {
-        this.createPortal(destination, -1);
+    /**
+     * Creates a permanent portal in the exit door leading to the destination.
+     * 
+     * @param destination The destination of the portal.
+     * @param dimension   The dimension of the destination.
+     */
+    public void createPortal(BlockPos destination, RegistryKey<World> dimension) {
+        this.createPortal(destination, dimension, -1);
     }
 
-    public void createPortal(BlockPos destination, int portalLifespan) {
+    /**
+     * Creates a temporary portal in the exit door leading to the destination.
+     * 
+     * @param destination    The destination of the portal.
+     * @param dimension      The dimension of the destination.
+     * @param portalLifespan The time, in ticks, that the portal will exist for.
+     */
+    public void createPortal(BlockPos destination, RegistryKey<World> dimension, int portalLifespan) {
         this.destination = destination;
+        this.destinationDimension = dimension;
         this.portalLifespan = portalLifespan;
     }
 
+    /**
+     * Clears the portal state of the exit door.
+     */
     public void removePortal() {
         this.destination = null;
-        this.portalLifespan = 0;
+        this.destinationDimension = null;
+        this.portalLifespan = -1;
     }
 }
